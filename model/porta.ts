00ad699ef058f31a6ec1d3240787a26a6f8b4569
Heitor Lima Moreira <61924603+heitorlimamorei@users.noreply.github.com>
2023-04-03 17:11:14 -0300

    fist commit

---
+++ model/porta.ts
@@ -0,0 +1,70 @@
+export default class PortaModel {
+  #numero: number;
+  #temPresente: boolean;
+  #selecionada: boolean;
+  #aberta: boolean;
+
+  constructor(
+    numero: number,
+    temPresente = false,
+    selecionada = false,
+    aberta = false
+  ) {
+    this.#numero = numero;
+    this.#temPresente = temPresente;
+    this.#selecionada = selecionada;
+    this.#aberta = aberta;
+  }
+  static getWhitePort() {
+    return new PortaModel(0, false, false, false);
+  }
+  get numero() {
+    return this.#numero;
+  }
+
+  get temPresente() {
+    return this.#temPresente;
+  }
+
+  get selecionada() {
+    return this.#selecionada;
+  }
+
+  get aberta() {
+    return this.#aberta;
+  }
+
+  get fechada() {
+    return !this.aberta;
+  }
+
+  desselecionar() {
+    const selecionada = false;
+    return new PortaModel(
+      this.numero,
+      this.temPresente,
+      selecionada,
+      this.aberta
+    );
+  }
+
+  alternarSelecao() {
+    const selecionada = !this.selecionada;
+    return new PortaModel(
+      this.numero,
+      this.temPresente,
+      selecionada,
+      this.aberta
+    );
+  }
+
+  abrir() {
+    const aberta = true;
+    return new PortaModel(
+      this.numero,
+      this.temPresente,
+      this.selecionada,
+      aberta
+    );
+  }
+}
